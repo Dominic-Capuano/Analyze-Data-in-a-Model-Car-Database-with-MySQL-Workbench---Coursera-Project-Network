56146528c8d26650cb8b3336c0f93d90bb99a699
Dominic-Capuano <167029131+Dominic-Capuano@users.noreply.github.com>
2024-04-27 17:17:29 -0400

    Add files via upload

--- Warehouse Capacities.sql
+++ Warehouse Capacities.sql
@@ -1,10 +1,11 @@
 # This query lists warehouse and capacity based on quantityInStock
-# as percent capacity as inventory space.
+# as percent capacity as inventory space. Ordered by Excess Capacity
 use mintclassics;
-SELECT warehouses.warehouseCode AS "CODE", warehouses.warehouseName AS "NAME", warehouses.warehousePctCap AS "% Capacity",
-sum(products.quantityInStock) AS "UNITS",
- Format(sum(products.quantityInStock/(warehouses.warehousePctCap/100)-products.quantityInStock),"##") AS "EXCESS",
- Format((sum(products.quantityInStock)/(warehouses.warehousePctCap/100)),"##") AS "MAX CAP"
+SELECT warehouses.warehouseCode AS "WAREHOUSE CODE",
+ warehouses.warehouseName AS "WAREHOUSE NAME", warehouses.warehousePctCap AS "% OF Capacity",
+Format(sum(products.quantityInStock),"##") AS "UNITS IN INVENTORY",
+ Format(sum(products.quantityInStock/(warehouses.warehousePctCap/100)-products.quantityInStock),"##") AS "EXCESS CAPACITY",
+ Format((sum(products.quantityInStock)/(warehouses.warehousePctCap/100)),"##") AS "MAXIMUM CAPACITY"
   FROM warehouses, products where warehouses.warehouseCode = products.warehouseCode
 GROUP BY warehouses.warehouseCode
 order by sum(products.quantityInStock/(warehouses.warehousePctCap/100)-products.quantityInStock) desc /* EXCESS */
