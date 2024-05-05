6c98bd44769298272553971a783e15b0bd53d7f1
Dominic-Capuano <167029131+Dominic-Capuano@users.noreply.github.com>
2024-05-05 10:56:09 -0400

    Delete Customers by Country.sql

--- Customers by Country.sql
+++
@@ -1,7 +0,0 @@
-# This query list customers by country
-# Grouped by Country Number of Customers Decending and Alpha by Customer within ties 
-use mintclassics;
-SELECT Country, COUNT(customerNumber) AS "Number of Customers"
-FROM customers
-GROUP BY Country
-ORDER BY COUNT(CustomerNumber) DESC, Country ASC;
